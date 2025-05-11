6e24280576c456219449fa3683349b1307dd326c
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2025-05-11 20:56:18 +0800

    Add signup UI

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { LockKeyhole } from "lucide-react";
 import PasswordField from "@/components/ui/password-field";
 
-export default function Login() {
+export default function Page() {
     return (
         <form className="flex h-lvh items-center justify-center">
             <div className="w-full max-w-md space-y-6 px-4 lg:px-0">
@@ -19,23 +19,27 @@ export default function Login() {
                     </h5>
                 </div>
                 <div className="space-y-2">
-                    <label htmlFor="email" className="block font-medium">
-                        Email
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        placeholder="Enter email address"
-                        className="text-primary-text w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
-                    />
-                    <label htmlFor="password" className="block font-medium">
-                        Password
-                    </label>
-                    <PasswordField
-                        id="password"
-                        placeholder="Enter password"
-                        className="text-primary-text absolute w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
-                    />
+                    <div>
+                        <label htmlFor="email" className="block font-medium">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter email address"
+                            className="text-primary-text w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block font-medium">
+                            Password
+                        </label>
+                        <PasswordField
+                            id="password"
+                            placeholder="Enter password"
+                            className="text-primary-text absolute w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
                     <div className="flex justify-between">
                         <div className="flex items-center gap-2">
                             <input
@@ -69,8 +73,8 @@ export default function Login() {
                     <p className="text-secondary-text pt-4 text-sm">
                         Don't have an account yet?{" "}
                         <Link
-                            href="#"
-                            className="text-secondary-dark font-medium hover:underline"
+                            href="/signup"
+                            className="text-secondary-dark pl-1 font-medium hover:underline"
                         >
                             Sign up here
                         </Link>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,102 @@
-export default function Signup() {
-    return <>Sign up</>;
+import Link from "next/link";
+import { LockKeyhole } from "lucide-react";
+import PasswordField from "@/components/ui/password-field";
+
+export default function Page() {
+    return (
+        <form className="flex h-lvh items-center justify-center">
+            <div className="w-full max-w-md space-y-6 px-4 lg:px-0">
+                <div>
+                    <div className="flex items-center gap-1">
+                        <h2 className="font-semibold">Signup to dashboard</h2>
+                        <LockKeyhole
+                            className="text-secondary-main"
+                            size={24}
+                        />
+                    </div>
+                    <h5 className="text-secondary-text">
+                        Fill the below form to login
+                    </h5>
+                </div>
+                <div className="space-y-4">
+                    <div>
+                        <label htmlFor="username" className="block font-medium">
+                            Username
+                        </label>
+                        <input
+                            id="username"
+                            type="text"
+                            placeholder="Enter username"
+                            className="text-primary-text w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block font-medium">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter email address"
+                            className="text-primary-text w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block font-medium">
+                            Password
+                        </label>
+                        <PasswordField
+                            id="password"
+                            placeholder="Enter password"
+                            className="text-primary-text absolute w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="confirm-password"
+                            className="block font-medium"
+                        >
+                            Confirm Password
+                        </label>
+                        <PasswordField
+                            id="confirm-password"
+                            placeholder="Confirm Password"
+                            className="text-primary-text absolute w-full rounded-sm border-2 border-gray-200 p-2 outline-blue-300 placeholder:font-light placeholder:text-gray-300"
+                        />
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <input
+                            id="remember-me"
+                            type="checkbox"
+                            className="checkbox-primary-main"
+                        />
+                        <label
+                            htmlFor="remember-me"
+                            className="cursor-pointer select-none"
+                        >
+                            Remember me
+                        </label>
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        className="bg-primary-main hover:bg-primary-dark w-full cursor-pointer rounded p-2 text-white transition-colors duration-500"
+                    >
+                        Login
+                    </button>
+                    <p className="text-secondary-text pt-4 text-sm">
+                        Already have an account?
+                        <Link
+                            href="/"
+                            className="text-secondary-dark pl-1 font-medium hover:underline"
+                        >
+                            Login here
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </form>
+    );
 }
