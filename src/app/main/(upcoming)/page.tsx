97bd166b6e1484e54bc923ff24dbb4142a644385
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2025-05-25 17:31:14 +0800

    feature/todo-item

--- src/app/main/(upcoming)/page.tsx
+++ src/app/main/(upcoming)/page.tsx
@@ -1,7 +1,13 @@
+import TodoItem from "@/components/shared/todo-item";
+
 export default function Page() {
     return (
         <div className="pl-4">
             <h1>Upcoming</h1>
+            <TodoItem
+                name="Creating something that is todo"
+                group={{ name: "Work", color: "#00b7fd" }}
+            />
         </div>
     );
 }
